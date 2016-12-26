e7d52fa812d68f492d2424126ad7fb7726a27ab7
wangzhaob <wangzhaob@chanjet.com>
2016-12-26 14:26:34 +0800

    add v2.0 Customer testcase
    Change-Id: Ie8736b48ec500f7f005a722ab4341ebdf9823fa6
    Signed-off-by: wangzhaob <wangzhaob@chanjet.com>

---
+++ app-import-test/src/test/java/com/cspdemo/testapp/test/v2_0/CustomerIT.java
@@ -0,0 +1,28 @@
+package com.cspdemo.testapp.test.v2_0;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.Test;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import com.chanjet.csp.platform.test.HttpResponse;
+import com.chanjet.csp.platform.test.RestfulIT;
+
+public class CustomerIT extends RestfulIT {
+	@Test
+	public void shouldAllowAddANewCustomer() throws Exception {
+		Map<String, Object> bar = new HashMap<String, Object>();
+		bar.put("name", "bar");
+		setIsCspServletURL(false);
+		HttpResponse response = this.doPost("isvtest", "/restlet/v2_0/Customer", bar);
+		assertEquals(200, response.getStatusCode());
+	    String barString = response.getString();
+	    JSONObject barJson = JSON.parseObject(barString);
+	    //System.out.println(barJson);
+	    assertEquals("bar", barJson.get("name"));
+	}
+}
