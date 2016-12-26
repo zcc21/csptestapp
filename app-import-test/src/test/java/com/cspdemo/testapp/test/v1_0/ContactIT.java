dce6d09a49764b2f9a7b9cdaedfdb630a1951513
wangzhaob <wangzhaob@chanjet.com>
2016-12-26 17:00:49 +0800

    curd testcase
    Change-Id: I93b0845133eb9a27421eb9be58cc801461d83c60
    Signed-off-by: wangzhaob <wangzhaob@chanjet.com>

--- app-import-test/src/test/java/com/cspdemo/testapp/test/v1_0/ContactIT.java
+++ app-import-test/src/test/java/com/cspdemo/testapp/test/v1_0/ContactIT.java
@@ -1,6 +1,6 @@
 package com.cspdemo.testapp.test.v1_0;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -21,27 +21,69 @@ public class ContactIT extends RestfulIT {
 	public void setUp() throws Exception {
 		Map<String, Object> fooMap = new HashMap<String, Object>();
 		fooMap.put("name", DEFAULT_FOO_NAME);
+		
+		// POST - http://localhost:8080/cspdemo/testapp/services/1.0/bo/dml/{boName}
 		HttpResponse response = this.doPost("isvtest", "/bo/dml/Contact", fooMap);
 		assertEquals(200, response.getStatusCode());
+		
 	    String fooString = response.getString();
 	    foo = JSON.parseObject(fooString);
 	}
 	
 	@Test
-	public void shouldAllowAddANewContact() throws Exception {		
-	    //System.out.println(fooJson);
+	public void shouldAllowAddANewContact() throws Exception {
+		// Check
+		assertNotNull(foo.get("id"));
 	    assertEquals(DEFAULT_FOO_NAME, foo.get("name"));	   
 	}
 	
 	@Test
-	public void shouldAllowUpdateAContact() throws Exception {		
-		// Map<String, Object> foo2Map = new HashMap<String, Object>();
-		String foo2Name = "foo2";
-		foo.put("name", foo2Name);
-		HttpResponse response = this.doPut("isvtest", "bo/dml/Contact/" + foo.get("id"), foo);
+	public void shouldAllowUpdateAContact() throws Exception {
+		// Given:
+		Long fooId = foo.getLongValue("id");
+		String newFooName = "foofoo";
+		foo.put("name", newFooName);
+		
+		// When: update the name
+		// PUT - http://localhost:8080/cspdemo/testapp/services/1.0/bo/dml/{boName}/{ID}
+		HttpResponse response = this.doPut("isvtest", "bo/dml/Contact/" + fooId, foo);
 		assertEquals(200, response.getStatusCode());
-	    String fooString = response.getString();
-	    JSONObject fooJson = JSON.parseObject(fooString); 
-	    assertEquals(foo2Name, fooJson.get("name"));
+	    
+	    // Then: check whether the name has updated
+	    response = this.doGet("isvtest", "bo/query/Contact/" + fooId, null);		
+	    String updatedFooString = response.getString();
+	    JSONObject updatedFoo = JSON.parseObject(updatedFooString);
+	    assertEquals(newFooName, updatedFoo.get("name"));
+	}
+	
+	@Test
+	public void shouldAllowGetAContact() throws Exception {
+		// Given:
+		Long fooId = foo.getLongValue("id");
+		
+		// When: reload the foo
+		// GET - http://localhost:8080/cspdemo/testapp/services/1.0/bo/query/{boName}/{ID}
+		HttpResponse response = this.doGet("isvtest", "bo/query/Contact/" + fooId, null);
+		assertEquals(200, response.getStatusCode());
+	    String sameFooString = response.getString();
+	    JSONObject sameFoo = JSON.parseObject(sameFooString);
+	    
+	    // Then: check whether the two foos are same
+	    assertEquals(fooId, sameFoo.getLong("id"));
+	    assertEquals(foo.get("name"), sameFoo.get("name"));
+	}
+	
+	@Test
+	public void shouldAllowDeleteAContact() throws Exception {
+		// Given:
+		Long fooId = foo.getLongValue("id");
+		
+		// When: delete the foo
+		// DELETE - http://localhost:8080/cspdemo/testapp/services/1.0/bo/dml/{boName}/{ID}
+		HttpResponse response = this.doDelete("isvtest", "bo/dml/Contact/" + fooId, null);
+		
+		// Then: check whether the foo is deleted
+		response = this.doGet("isvtest", "bo/query/Contact/" + fooId, null);
+		assertEquals(400, response.getStatusCode());	
 	}
 }
--- app-import-test/src/test/java/com/cspdemo/testapp/test/v1_0/CustomerIT.java
+++ app-import-test/src/test/java/com/cspdemo/testapp/test/v1_0/CustomerIT.java
@@ -1,10 +1,12 @@
 package com.cspdemo.testapp.test.v1_0;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 import java.util.HashMap;
 import java.util.Map;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import com.alibaba.fastjson.JSON;
@@ -13,15 +15,76 @@ import com.chanjet.csp.platform.test.HttpResponse;
 import com.chanjet.csp.platform.test.RestfulIT;
 
 public class CustomerIT extends RestfulIT {
-	@Test
-	public void shouldAllowAddANewCustomer() throws Exception {		
-		Map<String, Object> bar = new HashMap<String, Object>();
-		bar.put("name", "bar");	
-		HttpResponse response = this.doPost("isvtest", "/bo/dml/Customer", bar);
+	private JSONObject bar;
+	private static final String DEFAULT_BAR_NAME = "bar";
+	
+	@Before
+	public void setUp() throws Exception {
+		Map<String, Object> barMap = new HashMap<String, Object>();
+		barMap.put("name", DEFAULT_BAR_NAME);
+		
+		// POST - http://localhost:8080/cspdemo/testapp/services/1.0/bo/dml/{boName}
+		HttpResponse response = this.doPost("isvtest", "/bo/dml/Customer", barMap);
 		assertEquals(200, response.getStatusCode());
+		
 	    String barString = response.getString();
-	    JSONObject barJson = JSON.parseObject(barString);
-	    //System.out.println(barJson);
-	    assertEquals("bar", barJson.get("name"));	   
+	    bar = JSON.parseObject(barString);
+	}
+	
+	@Test
+	public void shouldAllowAddANewCustomer() throws Exception {
+		// Check
+		assertNotNull(bar.get("id"));
+	    assertEquals(DEFAULT_BAR_NAME, bar.get("name"));	   
+	}
+	
+	@Test
+	public void shouldAllowUpdateACustomer() throws Exception {
+		// Given:
+		Long barId = bar.getLongValue("id");
+		String newBarName = "barbar";
+		bar.put("name", newBarName);
+		
+		// When: update the name
+		// PUT - http://localhost:8080/cspdemo/testapp/services/1.0/bo/dml/{boName}/{ID}
+		HttpResponse response = this.doPut("isvtest", "bo/dml/Customer/" + barId, bar);
+		assertEquals(200, response.getStatusCode());
+	    
+	    // Then: check whether the name has updated
+	    response = this.doGet("isvtest", "bo/query/Customer/" + barId, null);		
+	    String updatedBarString = response.getString();
+	    JSONObject updatedBar = JSON.parseObject(updatedBarString);
+	    assertEquals(newBarName, updatedBar.get("name"));
+	}
+	
+	@Test
+	public void shouldAllowGetACustomer() throws Exception {
+		// Given:
+		Long barId = bar.getLongValue("id");
+		
+		// When: reload the bar
+		// GET - http://localhost:8080/cspdemo/testapp/services/1.0/bo/query/{boName}/{ID}
+		HttpResponse response = this.doGet("isvtest", "bo/query/Customer/" + barId, null);
+		assertEquals(200, response.getStatusCode());
+	    String sameBarString = response.getString();
+	    JSONObject sameBar = JSON.parseObject(sameBarString);
+	    
+	    // Then: check whether the two bars are same
+	    assertEquals(barId, sameBar.getLong("id"));
+	    assertEquals(bar.get("name"), sameBar.get("name"));
+	}
+	
+	@Test
+	public void shouldAllowDeleteACustomer() throws Exception {
+		// Given:
+		Long barId = bar.getLongValue("id");
+		
+		// When: delete the bar
+		// DELETE - http://localhost:8080/cspdemo/testapp/services/1.0/bo/dml/{boName}/{ID}
+		HttpResponse response = this.doDelete("isvtest", "bo/dml/Customer/" + barId, null);
+		
+		// Then: check whether the bar is deleted
+		response = this.doGet("isvtest", "bo/query/Customer/" + barId, null);
+		assertEquals(400, response.getStatusCode());	
 	}
 }
