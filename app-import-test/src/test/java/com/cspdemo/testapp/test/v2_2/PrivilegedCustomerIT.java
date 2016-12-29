dae4a908d7662fc81d120d36be2dcd5e27035aa0
wangzhaob <wangzhaob@chanjet.com>
2016-12-29 16:51:16 +0800

    add url rule testcase
    Change-Id: Ieb7a38e972077ed720f5ffc552515052b3a4efca
    Signed-off-by: wangzhaob <wangzhaob@chanjet.com>

--- app-import-test/src/test/java/com/cspdemo/testapp/test/v2_2/PrivilegedCustomerIT.java
+++ app-import-test/src/test/java/com/cspdemo/testapp/test/v2_2/PrivilegedCustomerIT.java
@@ -2,6 +2,7 @@ package com.cspdemo.testapp.test.v2_2;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -10,6 +11,7 @@ import java.util.Map;
 
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 
@@ -18,7 +20,7 @@ import com.alibaba.fastjson.JSONObject;
 import com.chanjet.csp.platform.test.HttpResponse;
 import com.chanjet.csp.platform.test.RestfulIT;
 
-public class CustomerDataAuthIT extends RestfulIT {
+public class PrivilegedCustomerIT extends RestfulIT {
 
 	private CustomerService customerService = new CustomerService();
 	private static final String CUSTOMER_ENTITY_NAME = "Customer";
@@ -42,10 +44,12 @@ public class CustomerDataAuthIT extends RestfulIT {
 	private static final String UPDATE_PRIVILEGE = "UPDATE";
 	private static final String DELETE_PRIVILEGE = "DELETE";
 	// 
-	//private static 
+	private static final Long CUSTOMER_CUR_URL_RULE_ID = 10001L;
+	private static final Long CUSTOMER_DELETE_URL_RULE_ID = 10002L;
+	private List<Long> bossRoleUrlRuleIds = new ArrayList<>();	
 	
-	
-	private ExpectedException thrown = ExpectedException.none();
+	@Rule
+	public ExpectedException thrown = ExpectedException.none();
 	
 	@Before
 	public void setUp() throws Exception {
@@ -69,6 +73,40 @@ public class CustomerDataAuthIT extends RestfulIT {
 		salesmanAssignmentIds = assignSalesmanPrivilegesTo(salesmanRoleId, CUSTOMER_ENTITY_NAME);		
 	}
 	
+	@After
+	public void tearDown() throws Exception {
+		setIsCspServletURL(true);
+		
+		deleteUserRoleOf(bossUserRoleId);
+		deleteUserRoleOf(salesmanUserRoleId);
+		
+		deleteAssignedPrivilegesOf(bossAssignmentIds);
+		deleteAssignedPrivilegesOf(salesmanAssignmentIds);
+		
+		deleteAssignedUrlRulesOf(bossRoleUrlRuleIds);
+		
+		deleteRoleOf(bossRoleId);
+		deleteRoleOf(salesmanRoleId);
+		
+		disableAppPrivilege();
+		disableEntityPrivilege(CUSTOMER_ENTITY_NAME);
+		disableUserPrivilege(bossId);
+		disableUserPrivilege(salesmanId);
+	}
+
+	private void deleteAssignedUrlRulesOf(List<Long> roleUrlRuleIds) throws Exception {
+		if (roleUrlRuleIds == null || roleUrlRuleIds.isEmpty()) {
+			return;
+		}
+		
+		for (Long roleUrlRuleId : roleUrlRuleIds) {
+			// DELETE - http://localhost:8080/cspdemo/testapp/services/1.0/dataauth/roleurlrule/{roleUrlRuleId}
+			HttpResponse response = this.doDelete("admin", "/dataauth/roleurlrule/" + roleUrlRuleId, null);
+			assertEquals(200, response.getStatusCode());	
+		}	
+		
+	}
+
 	@Test
 	public void shouldPermitBossToAddNew() throws Exception {	
 		JSONObject bar = customerService.addNewBy(BOSS_NAME, DEFAULT_BAR_NAME);		
@@ -121,10 +159,8 @@ public class CustomerDataAuthIT extends RestfulIT {
 		Long barId = bar.getLong("id");
 		customerService.deleteBy(BOSS_NAME, barId);	
 
-		// Then: check the bar has been deleted
-		thrown.expect(RuntimeException.class);
-		thrown.expectMessage("status:400");
-		customerService.getBy(SALESMAN_NAME, barId);
+		// Then: check the bar has been deleted	
+		assertNull(customerService.getBy(SALESMAN_NAME, barId));
 	}
 	
 	@Test
@@ -145,8 +181,7 @@ public class CustomerDataAuthIT extends RestfulIT {
 	@Test
 	public void shouldNotPermitSalesmanToAddNew() throws Exception {
 		// disable the salesman to insert
-		thrown.expect(RuntimeException.class);
-		thrown.expectMessage("status:400");
+		thrown.expect(RuntimeException.class);		
 		customerService.addNewBy(SALESMAN_NAME, DEFAULT_BAR_NAME);
 	}
 	
@@ -162,8 +197,7 @@ public class CustomerDataAuthIT extends RestfulIT {
 		
 		// When & Check
 		// disable the salesman to update the bar created by the boss
-		thrown.expect(RuntimeException.class);
-		thrown.expectMessage("status:400");
+		thrown.expect(RuntimeException.class);	
 		Long barId = barCreatedByBoss.getLong("id");
 		customerService.updateBy(SALESMAN_NAME, barId, newBarMap);		
 	}
@@ -176,8 +210,7 @@ public class CustomerDataAuthIT extends RestfulIT {
 		
 		// When & Check
 		// disable the salesman to delete the bar created by the boss
-		thrown.expect(RuntimeException.class);
-		thrown.expectMessage("status:400");
+		thrown.expect(RuntimeException.class);		
 		Long barId = barCreatedByBoss.getLong("id");
 		customerService.deleteBy(SALESMAN_NAME, barId);		
 	}
@@ -214,19 +247,85 @@ public class CustomerDataAuthIT extends RestfulIT {
 		Long barId = barCreatedByBoss.getLong("id");
 		assertNotNull(barId);
 		// grant delete privilege of the bar to salesman by boss
-		Long grantId = grant(barId, BOSS_NAME, SALESMAN_NAME, CUSTOMER_ENTITY_NAME, DELETE_PRIVILEGE);
+		grant(barId, BOSS_NAME, SALESMAN_NAME, CUSTOMER_ENTITY_NAME, DELETE_PRIVILEGE);
 		
 		// When:	
 		// enable the salesman to update the bar	
 		customerService.deleteBy(BOSS_NAME, barId);	
 
-		// Then: check the bar has been deleted
-		thrown.expect(RuntimeException.class);
-		thrown.expectMessage("status:400");
-		customerService.getBy(SALESMAN_NAME, barId);
+		// Then: check the bar has been deleted		
+		assertNull(customerService.getBy(SALESMAN_NAME, barId));
 		
 		// Tear down:
 		//deleteGrantOf(grantId, BOSS_NAME);
+	}
+	
+	@Test
+	public void shouldNotPermitBossToDeleteWhenHasNotDeleteUrlRule() throws Exception {
+		// Given:
+		// enable the data auth of url rule
+		enableUrlAccessRule();
+		// assign the CRD url rule privileges to the boss
+		bossRoleUrlRuleIds.add(assignUrlRuleTo(bossRoleId, CUSTOMER_CUR_URL_RULE_ID));
+		// add a new Customer
+		JSONObject bar = customerService.addNewBy(BOSS_NAME, DEFAULT_BAR_NAME);		
+				
+		// When & Check
+		// the boss cannot delete the bar, without the delete url rule
+		thrown.expect(RuntimeException.class);	
+		Long barId = bar.getLong("id");
+		customerService.deleteBy(BOSS_NAME, barId);		
+	}
+	
+	@Test
+	public void shouldPermitBossToDeleteWhenHasDeleteUrlRule() throws Exception {
+		// Given:
+		// enable the data auth of url rule
+		enableUrlAccessRule();
+		// assign the CRD url rule privileges to the boss
+		bossRoleUrlRuleIds.add(assignUrlRuleTo(bossRoleId, CUSTOMER_CUR_URL_RULE_ID));		
+		// add a new Customer
+		JSONObject bar = customerService.addNewBy(BOSS_NAME, DEFAULT_BAR_NAME);		
+				
+		// When:
+		// assign the delete url rule privileges to the boss
+		bossRoleUrlRuleIds.add(assignUrlRuleTo(bossRoleId, CUSTOMER_DELETE_URL_RULE_ID));
+		// the boss can delete the bar, with the delete url rule
+		Long barId = bar.getLong("id");
+		customerService.deleteBy(BOSS_NAME, barId);		
+		
+		// Then: check the bar has been deleted
+		//thrown.expect(RuntimeException.class);
+		//thrown.expectMessage("status:400");
+		JSONObject nullBar = customerService.getBy(SALESMAN_NAME, barId);
+		assertNull(nullBar);		
+			
+	}
+
+	private Long assignUrlRuleTo(Long roleId, Long urlRuleId) throws Exception {
+		JSONObject roleUrlRuleJson = new JSONObject();
+		roleUrlRuleJson.put("roleId", roleId);
+		roleUrlRuleJson.put("urlRuleId", urlRuleId);
+		
+		JSONArray roleUrlRules = new JSONArray();
+		roleUrlRules.add(roleUrlRuleJson);
+		
+		setIsCspServletURL(true);
+		// POST - http://localhost:8080/cspdemo/testapp/services/1.0/dataauth/roleurlrule
+		HttpResponse response = this.doPost("admin", "/dataauth/roleurlrule/", roleUrlRules);
+		assertEquals(200, response.getStatusCode());
+		
+		String roleUrlRulesString = response.getString();
+		Long roleUrlRuleId = JSONArray.parseArray(roleUrlRulesString).getJSONObject(0).getLong("id");
+		assertNotNull(roleUrlRuleId);
+		return roleUrlRuleId;	
+		
+	}
+
+	private void enableUrlAccessRule() throws Exception {
+		// PUT - http://localhost:8080/cspdemo/testapp/services/1.0/dataauth/urlaccess/{status}
+		HttpResponse response = this.doPut("admin", "/dataauth/urlaccess/enable", null);
+		assertEquals(200, response.getStatusCode());
 	}
 
 	private Long grant(Long objectId, String granter, String grantee, String entityName, String privilege) throws Exception {
@@ -245,10 +344,7 @@ public class CustomerDataAuthIT extends RestfulIT {
 		HttpResponse response = this.doPost(granter, "/dataauth/grants/", grantMap);
 		assertEquals(200, response.getStatusCode());
 		
-		String grantString = response.getString();
-		Long grantId = JSONObject.parseObject(grantString).getLong("id");
-		assertNotNull(grantId);
-		return grantId;
+		return getIdFrom(response);
 	}
 
 	private void deleteGrantOf(Long grantId, String granter) throws Exception {
@@ -263,26 +359,6 @@ public class CustomerDataAuthIT extends RestfulIT {
 		assertEquals(200, response.getStatusCode());		
 	}
 
-	@After
-	public void tearDown() throws Exception {
-		setIsCspServletURL(true);
-		
-		deleteUserRoleOf(bossUserRoleId);
-		deleteUserRoleOf(salesmanUserRoleId);
-		
-		deleteAssignedPrivilegesOf(bossAssignmentIds);
-		deleteAssignedPrivilegesOf(salesmanAssignmentIds);
-		
-		deleteRoleOf(bossRoleId);
-		deleteRoleOf(salesmanRoleId);
-		
-		disableAppPrivilege();
-		disableEntityPrivilege(CUSTOMER_ENTITY_NAME);
-		disableUserPrivilege(bossId);
-		disableUserPrivilege(salesmanId);
-	}
-	
-	
 	// boss role has all privileges, including insert, select, update, delete
 	private List<Long> assignBossPrivilegesTo(Long roleId, String entityName) throws Exception {
 		List<Long> assignmentIds = new ArrayList<>();
@@ -320,10 +396,7 @@ public class CustomerDataAuthIT extends RestfulIT {
 		HttpResponse response = this.doPost("admin", "/dataauth/assignments/role/" + roleId, assignmentMap);
 		assertEquals(200, response.getStatusCode());
 		
-		String assingmentString = response.getString();
-		Long assingmentId = JSONObject.parseObject(assingmentString).getLong("id");
-		assertNotNull(assingmentId);
-		return assingmentId;
+		return getIdFrom(response);
 	}
 
 	private void deleteAssignedPrivilegesOf(List<Long> assignmentIds) throws Exception {
@@ -383,10 +456,7 @@ public class CustomerDataAuthIT extends RestfulIT {
 		HttpResponse response = this.doPost("admin", "/dataauth/role/", roleMap);
 		assertEquals(200, response.getStatusCode());
 		
-	    String roleString = response.getString();
-		Long roleId = JSONObject.parseObject(roleString).getLong("id");
-		assertNotNull(roleId);
-		return roleId;
+	    return getIdFrom(response);
 	}
 
 	private void deleteRoleOf(Long roleId) throws Exception {
@@ -425,5 +495,12 @@ public class CustomerDataAuthIT extends RestfulIT {
 		HttpResponse response = this.doDelete("admin", "/dataauth/userrole/" + userRoleId, null);		
 		assertEquals(200, response.getStatusCode());
 
+	}
+
+	private Long getIdFrom(HttpResponse response) {
+		String objectString = response.getString();
+		Long objectId = JSONObject.parseObject(objectString).getLong("id");
+		assertNotNull(objectId);
+		return objectId;
 	}
 }
