9f6687f9ef0b2c5e388a2336054430827a8079c4
wangzhaob <wangzhaob@chanjet.com>
2016-12-29 16:52:02 +0800

    throw exception when bad request
    Change-Id: I44fa3330eb1650391ea0dfcdc0eb86ce3bd8ae0d
    Signed-off-by: wangzhaob <wangzhaob@chanjet.com>

--- app-import-test/src/test/java/com/cspdemo/testapp/test/v2_2/CustomerService.java
+++ app-import-test/src/test/java/com/cspdemo/testapp/test/v2_2/CustomerService.java
@@ -9,17 +9,25 @@ import com.chanjet.csp.platform.test.HttpResponse;
 import com.chanjet.csp.platform.test.RestfulIT;
 
 public class CustomerService extends RestfulIT {
+	private static final int BAD_REQUEST = 400;
+
 	public JSONObject addNewBy(String userTag, String name) throws Exception {
 		setIsCspServletURL(false);
 		
 		Map<String, Object> customerMap = new HashMap<String, Object>();
 		customerMap.put("name", name);
+		JSONObject customer = null; 
+		
 		// POST - http://localhost:8080/cspdemo/testapp/restlet/v2_1/{restletName} 
 		HttpResponse response = this.doPost(userTag, "/restlet/v2_1/Customer", customerMap);
-			
-	    String customerString = response.getString();
-	    JSONObject customer = JSON.parseObject(customerString);
-	    return customer;
+		
+		if (BAD_REQUEST == response.getStatusCode()) {
+			throw new RuntimeException(response.getString());
+		}
+		
+		String customerString = response.getString();
+		customer = JSON.parseObject(customerString);
+		return customer;
 	}
 
 	public JSONObject updateBy(String userTag, Long customerId, Map<String, Object> updatedCustomerMap) throws Exception {
@@ -27,6 +35,9 @@ public class CustomerService extends RestfulIT {
 	
 		// PUT - http://localhost:8080/cspdemo/testapp/restlet/v2_1/{restletName}/{ID}
 		HttpResponse response = this.doPut(userTag, "/restlet/v2_1/Customer/" + customerId, updatedCustomerMap);
+		if (BAD_REQUEST == response.getStatusCode()) {
+			throw new RuntimeException(response.getString());
+		}
 		
 		String updatedCustomerString = response.getString();
 	    JSONObject updatedCustomer = JSON.parseObject(updatedCustomerString);
@@ -38,6 +49,9 @@ public class CustomerService extends RestfulIT {
 		
 		// GET - http://localhost:8080/cspdemo/testapp/restlet/v2_1/{restletName}/{ID}
 		HttpResponse response = this.doGet(userTag, "/restlet/v2_1/Customer/" + customerId, null);
+		if (BAD_REQUEST == response.getStatusCode()) {
+			throw new RuntimeException(response.getString());
+		}
 		
 		String customerString = response.getString();
 	    JSONObject customer = JSON.parseObject(customerString);
@@ -45,9 +59,13 @@ public class CustomerService extends RestfulIT {
 	}
 
 	public void deleteBy(String userTag, Long barId) throws Exception {
-		setIsCspServletURL(false);
+		setIsCspServletURL(false);		
 		
 		// DELETE - http://localhost:8080/cspdemo/testapp/restlet/v2_1/{restletName}/{ID}
-		this.doDelete(userTag, "/restlet/v2_1/Customer/" + barId, null);		
+		HttpResponse response = this.doDelete(userTag, "/restlet/v2_1/Customer/" + barId, null);
+		
+		if (BAD_REQUEST == response.getStatusCode()) {
+			throw new RuntimeException(response.getString());
+		}				
 	}
 }
