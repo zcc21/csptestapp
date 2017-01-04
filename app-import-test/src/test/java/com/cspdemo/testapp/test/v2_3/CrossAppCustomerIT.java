fbf0d310b8ac63f50ca7c57265cb36606157042e
wangzhaob <wangzhaob@chanjet.com>
2017-01-04 13:45:14 +0800

    diable to delete big customer in app event
    Change-Id: I0248c16495bb8ec69e146b34ed198fcc541b44ee
    Signed-off-by: wangzhaob <wangzhaob@chanjet.com>

---
+++ app-import-test/src/test/java/com/cspdemo/testapp/test/v2_3/CrossAppCustomerIT.java
@@ -0,0 +1,47 @@
+package com.cspdemo.testapp.test.v2_3;
+
+import static org.junit.Assert.*;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+import com.alibaba.fastjson.JSONObject;
+import com.chanjet.csp.platform.test.RestfulIT;
+import com.cspdemo.testapp.test.utils.CustomerService;
+
+public class CrossAppCustomerIT extends RestfulIT {
+	private static final Long EMPLOYEE_NUMBER_OF_BIG_CUSTOMER = 1001L;
+	private static final Long EMPLOYEE_NUMBER_OF_SMALL_CUSTOMER = 101L;
+	private CustomerService customerService = new CustomerService();
+	
+	@Rule
+	public ExpectedException thrown = ExpectedException.none();
+	
+	@Test
+	public void shouldNotAllowDeleteBigCustomer() throws Exception {
+		// Given: add a big customer
+		JSONObject bar = customerService.addNewBy("admin", "bar", EMPLOYEE_NUMBER_OF_BIG_CUSTOMER);
+		Long barId = bar.getLong("id");
+		assertNotNull(barId);
+		
+		// When & Check
+		// Disable to delete the big customer in event handler
+		thrown.expect(RuntimeException.class);		
+		customerService.deleteBy("admin", barId);		
+	}
+	
+	@Test
+	public void shouldAllowDeleteSmallCustomer() throws Exception {
+		// Given: add a small customer
+		JSONObject bar = customerService.addNewBy("admin", "bar", EMPLOYEE_NUMBER_OF_SMALL_CUSTOMER);
+		Long barId = bar.getLong("id");
+		assertNotNull(barId);
+		
+		// When: delete the small bar
+		customerService.deleteBy("admin", barId);
+		
+		// Then: check the small bar has been deleted
+		assertNull(customerService.getBy("admin", barId));				
+	}
+}
--- webapp/src/main/java/com/chanapp/cspdemo/testapp/eventhandler/CustomSystemEventHandler.java
+++ webapp/src/main/java/com/chanapp/cspdemo/testapp/eventhandler/CustomSystemEventHandler.java
@@ -2,22 +2,41 @@ package com.chanapp.cspdemo.testapp.eventhandler;
 
 import java.util.List;
 
+import com.chanapp.cspdemo.testapp.service.CustomerService;
+import com.chanapp.cspdemo.testapp.service.CustomerServiceImpl;
 import com.chanjet.csp.bo.api.BoSession;
+import com.chanjet.csp.bo.api.IBusinessObjectRow;
 import com.chanjet.csp.cmr.api.metadata.userschema.type.businessObject.IBusinessObject;
 import com.chanjet.csp.cmr.api.metadata.userschema.type.entity.IEntity;
 import com.chanjet.csp.ess.api.messageBus.BOEventPayload;
 import com.chanjet.csp.event.SystemEventHandler;
 
-public class CustomSystemEventHandler extends SystemEventHandler {
+public class CustomSystemEventHandler extends SystemEventHandler {	
 	@Override
 	public boolean checkCanDelete(BoSession session, String sourceAppId, IEntity entity, Long rowId,
-			List<IBusinessObject> affectedBoList) {
-		return true;
+			List<IBusinessObject> affectedBoList) {		
+		boolean canDelete = true;		
+		if (entity.getName().equals("Customer")) {	
+			CustomerService customerService = new CustomerServiceImpl();			
+			if (customerService.isBigCustomer(rowId)) {
+				canDelete = false;
+			}			
+		}
+		
+		return canDelete;
+	}
+	
+	@Override
+	public void handlePreDelete(BoSession session, List<IBusinessObject> bos, BOEventPayload eventPayload, Long rowid,
+			Long userId, String appId) {		
+		System.out.println("pre delete");
 	}
 
 	@Override
 	public void handlePostDelete(BoSession session, List<IBusinessObject> bos, BOEventPayload eventPayload, Long rowid,
 			Long userId, String appId) {
+		IBusinessObject bo = bos.get(0);
+		IBusinessObjectRow row = eventPayload.constructBORow(session, boManager, bo);
 		System.out.println(appId);
 	}
 }
