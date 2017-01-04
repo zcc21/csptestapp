34a6f6b59481f96947d1838d6065fda928661ad9
wangzhaob <wangzhaob@chanjet.com>
2017-01-04 11:13:37 +0800

    getByName() in Contact service
    Change-Id: If3991e0b19035683fe1af6932b551545281d0207
    Signed-off-by: wangzhaob <wangzhaob@chanjet.com>

--- webapp/src/main/java/com/chanapp/cspdemo/testapp/service/ContactService.java
+++ webapp/src/main/java/com/chanapp/cspdemo/testapp/service/ContactService.java
@@ -3,12 +3,15 @@ package com.chanapp.cspdemo.testapp.service;
 import java.util.LinkedHashMap;
 
 import com.chanapp.cspdemo.testapp.businessobject.api.contact.IContactRow;
+import com.chanapp.cspdemo.testapp.businessobject.api.contact.IContactRowSet;
 
 public interface ContactService {
 
 	public void deleteById(Long id);
 
 	public IContactRow getById(Long id);
+	
+	public IContactRowSet getByName(String name);
 
 	public IContactRow addNew(LinkedHashMap<String, Object> contactMap);
 
--- webapp/src/main/java/com/chanapp/cspdemo/testapp/service/ContactServiceImpl.java
+++ webapp/src/main/java/com/chanapp/cspdemo/testapp/service/ContactServiceImpl.java
@@ -3,12 +3,15 @@ package com.chanapp.cspdemo.testapp.service;
 import java.util.LinkedHashMap;
 
 import com.chanapp.cspdemo.testapp.businessobject.api.contact.IContactRow;
+import com.chanapp.cspdemo.testapp.businessobject.api.contact.IContactRowSet;
 import com.chanjet.csp.appmanager.AppWorkManager;
 import com.chanjet.csp.bo.api.BoSession;
 import com.chanjet.csp.bo.api.BoTransactionManager;
 import com.chanjet.csp.bo.api.IBusinessObjectHome;
 import com.chanjet.csp.bo.api.IBusinessObjectManager;
 import com.chanjet.csp.common.base.util.TransactionTracker;
+import com.chanjet.csp.ui.util.Criteria;
+import com.chanjet.csp.ui.util.JsonQueryBuilder;
 
 public class ContactServiceImpl implements ContactService {
 
@@ -58,6 +61,37 @@ public class ContactServiceImpl implements ContactService {
 		}
 		return contactRow;
 	}
+
+	@Override
+	public IContactRowSet getByName(String name) {
+		BoSession boSession = AppWorkManager.getBoDataAccessManager().getBoSession();
+		BoTransactionManager transactionManager = AppWorkManager.getBoTransactionManager();
+		IContactRowSet contactRows = null;
+		try {
+			// open transaction
+			TransactionTracker transactionTrack = transactionManager.beginTransaction(boSession);
+			
+			// query contact by name
+			Criteria criteria = Criteria.AND();
+			criteria.eq("name", name);
+			
+			JsonQueryBuilder jsonQueryBuilder = JsonQueryBuilder.getInstance();		       
+			jsonQueryBuilder.addCriteria(criteria);
+			
+			IBusinessObjectManager boManager = AppWorkManager.getBusinessObjectManager();
+			IBusinessObjectHome boHome = boManager.getPrimaryBusinessObjectHome("Contact");		
+			contactRows = (IContactRowSet) boHome.query(jsonQueryBuilder.toJsonQuerySpec());		
+			
+			// commit
+			transactionManager.commitTransaction(boSession, transactionTrack);
+		} catch (Exception e) {
+			// roll back
+			transactionManager.rollbackTransaction(boSession);
+			throw new RuntimeException(e);
+		}
+		return contactRows;
+	}
+
 
 	@Override
 	public IContactRow addNew(LinkedHashMap<String, Object> contactMap) {
